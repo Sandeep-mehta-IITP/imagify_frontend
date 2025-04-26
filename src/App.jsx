a1adee597d681621f4eefcd87ab7662b8679a652
Sandeep-mehta-IITP <sandeep797671@gmail.com>
2025-04-26 11:05:30 +0530

    improve loader function for better user exprience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 import BuyCredit from "./pages/BuyCredit";
@@ -7,9 +7,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import { AppContext } from "./context/AppContext";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 import ForgotPassword from "./components/ForgotPassword";
-
 
 const FullScreenLoader = () => {
   return (
@@ -20,35 +19,40 @@ const FullScreenLoader = () => {
 };
 
 function App() {
-
-  const {showLogin, showForgotPassword} = useContext(AppContext)
+  const { showLogin, showForgotPassword } = useContext(AppContext);
   const [loading, setLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
-    // Simulate delay for splash screen, or do actual data fetching
-    const timer = setTimeout(() => setLoading(false), 1500); // or when everything is loaded
-    return () => clearTimeout(timer);
-  }, []);
+    setLoading(true);
 
-  if (loading) return <FullScreenLoader />;
+    // simulate page loading time or wait for data
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 300); // fast loader feel
 
+    return () => clearTimeout(timeout);
+  }, [location]);
 
   return (
-    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
-      <ToastContainer 
-      position="bottom-right"
-      />
-      <Navbar />
-      {showLogin && <Login />}
-      {showForgotPassword && <ForgotPassword />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/buy" element={<BuyCredit />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-      <Footer />
-    </div>
+    <>
+      {loading && <FullScreenLoader />}
+      {!loading && (
+        <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+          <ToastContainer position="bottom-right" />
+          <Navbar />
+          {showLogin && <Login />}
+          {showForgotPassword && <ForgotPassword />}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/result" element={<Result />} />
+            <Route path="/buy" element={<BuyCredit />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
